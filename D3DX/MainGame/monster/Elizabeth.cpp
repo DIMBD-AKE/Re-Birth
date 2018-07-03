bc9307fb45c4fcfa835ed709f1d4240970895123
preun <34766935+preun@users.noreply.github.com>
2018-07-03 16:35:44 +0900

    Revert "적이 튀어나오는구만?"
    This reverts commit 14c9262730d559590fa1b1f5dcddb3b9cb23b8a3.

--- D3DX/MainGame/monster/Elizabeth.cpp
+++ D3DX/MainGame/monster/Elizabeth.cpp
@@ -1,6 +1,6 @@
 #include "../../stdafx.h"
 #include "Elizabeth.h"
-#include "../Map.h"
+
 
 Elizabeth::Elizabeth()
 {
@@ -15,28 +15,19 @@ Elizabeth::~Elizabeth()
 
 void Elizabeth::Setup(Map* map, D3DXVECTOR3 spawnPos)
 {
-	MODELMANAGER->AddModel("엘리자베스", "Model/Enemy/Elizabeth/", "Elizabeth.x", MODELTYPE_X);
-	
-	m_pModel = MODELMANAGER->GetModel("엘리자베스", MODELTYPE_X);
-	//판정 박스 
-	ST_SIZEBOX box;
-	box.highX = 50.0f;
-	box.highY = 180.0f;
-	box.highZ = 50.0f;
-	box.lowX = -50.0f;
-	box.lowY = 10.0f;
-	box.lowZ = -50.0f;
-	
-	//모델 스캐일링
-
-	//m_pModel = new Model;
-	//스캐일링 먹이면 안나오는건지 이상한곳에 나오는건지 몰라서 일단 주석처리
-	//m_pModel->SetScale(D3DXVECTOR3(0.2f, 0.2f, 0.2f));
-
-	m_pModel->SetPosition(D3DXVECTOR3(spawnPos.x, map->GetHeight(spawnPos), spawnPos.z));
-
-	m_pModel->CreateBound(box);
-	m_pModel->SetBoundSphere(m_pModel->GetOrigBoundSphere().center, 100.0f);
+	//MODELMANAGER->AddModel("엘리자베스", "Model/Enemy/Elizabeth/", "Elizabeth.x", MODELTYPE_X);
+	//
+	////판정 박스 
+	//ST_SIZEBOX box;
+	//box.highX = 50.0f;
+	//box.highY = 180.0f;
+	//box.highZ = 50.0f;
+	//box.lowX = -50.0f;
+	//box.lowY = 10.0f;
+	//box.lowZ = -50.0f;
+	//
+	////모델 스캐일링
+	//m_pModel->SetScale(D3DXVECTOR3(0.02f, 0.02f, 0.02f));
 
 
 
@@ -48,21 +39,10 @@ void Elizabeth::Update()
 {
 
 	TEXT->Add("과연", 10, 10, 20);
-
-	if (m_pModel)
-	{
-		m_pModel->World();
-		m_pModel->Update();
-	}
 }
 
 void Elizabeth::Render()
 {
 	TEXT->Render();
-
-	if (m_pModel)
-	{
-		m_pModel->Render();
-	}
 }
 
