6177384126a3fcbb160d27d02b4b42f5efa39cfb
LoliWithMe <dream70521@gmail.com>
2018-07-04 17:41:44 +0900

    스텟 수정
    체력, 공격력 실수에서 정수로

--- D3DX/MainGame/Status.h
+++ D3DX/MainGame/Status.h
@@ -5,16 +5,16 @@ union STATUS
 	struct ST_STATUS_CHARACTER
 	{
 		// 아이템 스텟 + 캐릭터 스텟 = 능력치
-		float	fCurrentHP;		//캐릭터(적, 플레이어) 현재 피
-		float	fMaxHp;			//캐릭터(적, 플레이어) 최대 피
+		int		nCurrentHP;		//캐릭터(적, 플레이어) 현재 피
+		int		nMaxHp;			//캐릭터(적, 플레이어) 최대 피
 
-		float	fAtk;			//기본공격력
+		int		nAtk;			//기본공격력
 		float	fPhyRate;		//물리계수
 		float	fMagicRate;		//마법계수
 		float	fCheRate;		//화학계수
 		float	fAtkSpeed;		//공격속도 (장비속도에 * 연산)
 
-		float	fDef;			//방어력
+		int		nDef;			//방어력
 		float	fAgi;			//회피력
 		float	fHit;			//명중률
 		float	fSpeed;			//이동속도
@@ -26,9 +26,9 @@ union STATUS
 		float	fCoolTime1;		//스킬1 쿨타임
 		float	fCoolTime2;		//스킬2 쿨타임
 
-		float	fAtk;			//공격력 증가량
-		float	fDef;			//방어력 증가량
-		float	fHp;			//체력 증가량
+		int		nAtk;			//공격력 증가량
+		int		nDef;			//방어력 증가량
+		int		nHp;			//체력 증가량
 		float	fAgi;			//회피력 증가량
 		float	fHit;			//명중률 증가량
 		float	fSpeed;			//이동속도 증가량
@@ -37,7 +37,7 @@ union STATUS
 
 struct ST_DAMAGE
 {
-	float	fDamage;
+	int		nDamage;
 	float	fPhyRate;
 	float	fMagicRate;
 	float	fCheRate;
