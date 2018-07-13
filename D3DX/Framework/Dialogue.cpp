74d08bbd199eef6450bad62329cfe1982e576d7f
LoliWithMe <dream70521@gmail.com>
2018-07-14 05:16:25 +0900

    프레임워크 수정
    디아로그 클래스 추가

---
+++ D3DX/Framework/Dialogue.cpp
@@ -0,0 +1,71 @@
+#include "../stdafx.h"
+#include "Dialogue.h"
+
+Dialogue::Dialogue()
+{
+	ZeroMemory(this, sizeof(Dialogue));
+}
+
+
+Dialogue::~Dialogue()
+{
+}
+
+void Dialogue::Init(D3DXVECTOR2 pos, int size, string fontName, float interval, DWORD color)
+{
+	m_vPos = pos;
+	m_nSize = size;
+	m_sFont = fontName;
+	m_fInterval = interval;
+	m_nColor = color;
+}
+
+void Dialogue::SetKey(DWORD next, DWORD prev, DWORD rewrite)
+{
+	m_nNext = next;
+	m_nPrev = prev;
+	m_nReWrite = rewrite;
+}
+
+void Dialogue::Update()
+{
+	m_fElapse += TIME->GetElapsedTime();
+
+	if (INPUT->KeyDown(m_nReWrite))
+	{
+		m_nChar = 0;
+		m_nLine = 0;
+		m_vecCurrentText.clear();
+	}
+
+	if (m_vecCurrentText.empty())
+		m_vecCurrentText.push_back("");
+
+	if (m_fElapse > m_fPrevTime + m_fInterval)
+	{
+		m_fPrevTime = m_fElapse;
+
+		int size = 0;
+		for (int i = 0; i < m_vecCurrentText.size(); i++)
+			size += m_vecCurrentText[i].size();
+
+		if (size < m_vecAllText[m_nIndex].size())
+		{
+			if (m_vecAllText[m_nIndex][m_nChar] == '/')
+			{
+				m_vecCurrentText.push_back("");
+				m_nLine++;
+				m_nChar++;
+			}
+			else if (m_vecAllText[m_nIndex][m_nChar] & 0x80)
+				m_vecCurrentText[m_nLine] += m_vecAllText[m_nIndex][m_nChar++];
+			m_vecCurrentText[m_nLine] += m_vecAllText[m_nIndex][m_nChar];
+		}
+
+		if (m_vecAllText[m_nIndex].size() - 1 > m_nChar)
+			m_nChar++;
+	}
+
+	for (int i = 0; i < m_vecCurrentText.size(); i++)
+		TEXT->Add(m_vecCurrentText[i], m_vPos.x, m_vPos.y + m_nSize * i, m_nSize, m_sFont, m_nColor);
+}
---
+++ D3DX/Framework/Dialogue.h
@@ -0,0 +1,34 @@
+#pragma once
+class Dialogue
+{
+private:
+	vector<string>		m_vecAllText;
+	vector<string>		m_vecCurrentText;
+
+	D3DXVECTOR2			m_vPos;
+	int					m_nSize;
+	string				m_sFont;
+	float				m_fInterval;
+	DWORD				m_nColor;
+
+	int					m_nIndex;
+	int					m_nChar;
+	int					m_nLine;
+
+	DWORD				m_nNext;
+	DWORD				m_nPrev;
+	DWORD				m_nReWrite;
+
+	float				m_fElapse;
+	float				m_fPrevTime;
+
+public:
+	Dialogue();
+	~Dialogue();
+
+	void Init(D3DXVECTOR2 pos, int size, string fontName, float interval, DWORD color);
+	void SetKey(DWORD next, DWORD prev, DWORD rewrite);
+	void AddText(string text) { m_vecAllText.push_back(text); }
+	void Update();
+};
+
