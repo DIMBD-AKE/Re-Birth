51f7be5563dc70a05e13be1c972a5b8f34de697c
LoliWithMe <dream70521@gmail.com>
2018-07-19 05:08:47 +0900

    프레임워크 수정
    디아로그 오류 수정

--- D3DX/Framework/Dialogue.cpp
+++ D3DX/Framework/Dialogue.cpp
@@ -78,7 +78,7 @@ void Dialogue::Update()
 
 		if (size < m_vecAllText[m_nIndex].size())
 		{
-			if (m_vecAllText[m_nIndex][m_nChar] == '\n')
+			while (m_vecAllText[m_nIndex][m_nChar] == '\n')
 			{
 				m_vecCurrentText.push_back("");
 				m_nLine++;
