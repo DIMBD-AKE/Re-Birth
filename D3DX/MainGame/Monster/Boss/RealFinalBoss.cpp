c953f143308e47b3a529e70845c103d4aece754e
preun <34766935+preun@users.noreply.github.com>
2018-07-22 20:07:39 +0900

    진짜 최종보스 클래스 추가

---
+++ D3DX/MainGame/Monster/Boss/RealFinalBoss.cpp
@@ -0,0 +1,14 @@
+#include "RealFinalBoss.h"
+#include "../MonsterUseHeader.h"
+#include "../MonsterManager.h"
+#include "../Magic/MagicCircle.h"
+
+
+RealFinalBoss::RealFinalBoss()
+{
+}
+
+
+RealFinalBoss::~RealFinalBoss()
+{
+}
---
+++ D3DX/MainGame/Monster/Boss/RealFinalBoss.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Boss\BossParent.h"
+
+class RealFinalBoss :
+	public BossParent
+{
+public:
+	RealFinalBoss();
+	~RealFinalBoss();
+};
+
